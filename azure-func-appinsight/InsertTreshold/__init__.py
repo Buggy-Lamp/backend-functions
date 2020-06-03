3f1de7f648c3a5e75d7fed0fb61a9ca72a1d07e7
ta100rik <zyadosseyran@gmail.com>
2020-06-03 12:57:55 +0200

    insert fixed

--- azure-func-appinsight/InsertTreshold/__init__.py
+++ azure-func-appinsight/InsertTreshold/__init__.py
@@ -1,10 +1,8 @@
 import logging
 
 import azure.functions as func
-
 from azure.cosmos import exceptions, CosmosClient, PartitionKey
 from .. import family
-
 
 
 endpoint = "https://localhost:8081"
@@ -15,21 +13,20 @@ client = CosmosClient(endpoint, key)
 
 database = client.get_database_client(database_id)
 container = database.get_container_client(container_id)
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
-    tresholdAmount  = req.params.get('tresholdAmount')
-    tresholdName    = req.params.get('tresholdName')
-    if not tresholdAmount and not tresholdName:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            tresholdName    = req_body.get('tresholdName')
-            tresholdAmount  = req_body.get('tresholdAmount')
-    logging.info(tresholdAmount)
-    logging.info(tresholdName)
-    return func.HttpResponse(tresholdAmount+tresholdName)
-
+    req_body        = req.get_json()
+    tresholdAmount  = req_body.get('tresholdAmount')
+    tresholdName    = req_body.get('tresholdName')
+    tool            = req_body.get('tool')
+    instantiename   = req_body.get('instantiename')
+    
+    result = container.create_item(body=req_body)
+    logging.info(result);
+    if tresholdAmount:
+        return func.HttpResponse(f"{result}")
+    else:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
