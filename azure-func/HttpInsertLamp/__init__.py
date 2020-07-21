eee93a8f4e6149fc6794a7ba003d33d0c52d7ba1
Bart0110 <Bart0110@users.noreply.github.com>
2020-07-21 14:02:20 +0200

    [SQ][CODE-SMELL] Remove the unused local variable "o".

--- azure-func/HttpInsertLamp/__init__.py
+++ azure-func/HttpInsertLamp/__init__.py
@@ -9,6 +9,7 @@ from .. import family
 from .. import constants
 
 import json
+
 client = CosmosClient(constants.DB_ENDPOINT, constants.DB_KEY)
 
 database = client.get_database_client(constants.DB_DATABASE_ID)
@@ -16,19 +17,20 @@ container = database.get_container_client(constants.DB_LAMPCONTAINER_ID)
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    req_body        = family.generateid(req.get_json())
-    macadress       = req_body.get('mac')
-    
+    req_body = family.generateid(req.get_json())
+    macadress = req_body.get('mac')
+
     if macadress:
         result = container.create_item(body=req_body)
-        o = urlparse(req.url)
-        result["url"] = urljoin(req.url,"HttpGetLampData?code=53DiyE4/SdwJTN4ZcWFhL9PK4r115XbHBCB4XR8RpWvnvuJPYuxujA==&lampid=" +result['id'])
+        result["url"] = urljoin(req.url,
+                                "HttpGetLampData?code=53DiyE4/SdwJTN4ZcWFhL9PK4r115XbHBCB4XR8RpWvnvuJPYuxujA==&lampid="
+                                + result['id'])
     else:
         result = f"Not every condition is provided"
     if result:
         return func.HttpResponse(json.dumps(result))
     else:
         return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+            "Please pass a name on the query string or in the request body",
+            status_code=400
         )
