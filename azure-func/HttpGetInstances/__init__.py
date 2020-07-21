8050533905ef36e5d717156137f165edafd4b6c6
Bart0110 <Bart0110@users.noreply.github.com>
2020-07-21 14:11:58 +0200

    [SQ][CODE-SMELL] Simplify complexity

--- azure-func/HttpGetInstances/__init__.py
+++ azure-func/HttpGetInstances/__init__.py
@@ -16,18 +16,7 @@ states_container = database.get_container_client(DB_STATES_CONTAINER_ID)
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    project_id = req.params.get('project')
-    instance_name = req.params.get('instance_name')
-    if not project_id or not instance_name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            project_id = req_body.get('project') \
-                if req_body.get('project') is not None else project_id
-            instance_name = req_body.get('instance_name') \
-                if req_body.get('instance_name') is not None else instance_name
+    project_id, instance_name = extract_req_info(req)
 
     if not project_id:
         return func.HttpResponse("Project is a required field", status_code=400)
@@ -35,9 +24,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if '\'' in project_id or '"' in project_id or '\\' in project_id:
         return func.HttpResponse("Project is an invalid string", status_code=400)
 
-    if instance_name:
-        if '\'' in instance_name or '"' in project_id or '\\' in project_id:
-            return func.HttpResponse("Instance name is an invalid string", status_code=400)
+    if instance_name or '\'' in instance_name or '"' in project_id or '\\' in project_id:
+        return func.HttpResponse("Instance name is an invalid string", status_code=400)
 
     settings = list(container.query_items(query=f"SELECT * FROM c WHERE "
                                                 f"c.project = '{project_id}'",
@@ -67,6 +55,23 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse(json.dumps(settings), mimetype=HTTP_JSON_MIMETYPE)
 
 
+def extract_req_info(req: func.HttpRequest):
+    project_id = req.params.get('project')
+    instance_name = req.params.get('instance_name')
+    if not project_id or not instance_name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            project_id = req_body.get('project') \
+                if req_body.get('project') is not None else project_id
+            instance_name = req_body.get('instance_name') \
+                if req_body.get('instance_name') is not None else instance_name
+
+    return project_id, instance_name
+
+
 def attach_state(settings: dict, state: dict):
     settings['state_color'] = state['color']
 
@@ -78,18 +83,22 @@ def attach_state(settings: dict, state: dict):
         tool_state = state['tools'][tool['tool_name']]
         tool['state_color'] = tool_state['color']
 
-        for instance in tool['instances']:
-            if instance['instance_name'] not in tool_state:
+        attach_instance_state(tool['instances'], tool_state)
+
+
+def attach_instance_state(instances: dict, tool_state: dict):
+    for instance in instances:
+        if instance['instance_name'] not in tool_state:
+            continue
+
+        instance_state = tool_state[instance['instance_name']]
+        instance['state_color'] = instance_state['color']
+
+        for prop in instance['properties']:
+            if 'properties' not in instance_state:
+                continue
+            if prop['property_name'] not in instance_state['properties']:
                 continue
 
-            instance_state = tool_state[instance['instance_name']]
-            instance['state_color'] = instance_state['color']
-
-            for prop in instance['properties']:
-                if 'properties' not in instance_state:
-                    continue
-                if prop['property_name'] not in instance_state['properties']:
-                    continue
-
-                prop_state = instance_state['properties'][prop['property_name']]
-                prop['state_color'] = prop_state['color']
+            prop_state = instance_state['properties'][prop['property_name']]
+            prop['state_color'] = prop_state['color']
