358eab605f9d09cc7c942860744592df64961bd5
Bart <bart26nl@gmail.com>
2020-08-04 18:40:51 +0200

    empty file

--- azure-func-test/HttpGetExceptions.py
+++ azure-func-test/HttpGetExceptions.py
@@ -7,6 +7,11 @@ class TestHttpCalcState(unittest.TestCase):
     # looks like we can expect the same faulty responses as in GetAvailability
     # see https://discordapp.com/channels/681905163463688239/739865576523956357/740240599739007047
 
+    noParams = {}
+
+    def test_noParams(self):
+        result = create_GETParam(self.api, self.noParams)
+        self.assertEqual(result, 400)
 
 
 #So it can be run from commandline
