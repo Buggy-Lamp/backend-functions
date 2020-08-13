8e6d51ddc8d87642b39994304b1175d290e86a5a
ta100rik <zyadosseyran@gmail.com>
2020-08-13 19:34:07 +0200

    insert treshold test case created test not written because of question

---
+++ azure-func-test/TestHttpInsertTreshold.py
@@ -0,0 +1,13 @@
+import unittest
+
+from utilities.RequestGeneratorUtil import create_POST
+
+
+class TestHttpInsertTreshold(unittest.TestCase):
+
+    def Test_newinstance(self):
+        # httpRequestInvalidJson = {'': }
+
+
+if __name__ == '__main__':
+    unittest.main()
