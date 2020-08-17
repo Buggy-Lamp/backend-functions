08fa53193eff805752f05dfa836a136df24b6713
Bart <bart26nl@gmail.com>
2020-08-18 00:20:39 +0200

    "• Lege excepties in toolservices/exceptions.py"

--- azure-func/ToolServices/exceptions.py
+++ azure-func/ToolServices/exceptions.py
@@ -1,18 +1,24 @@
+import traceback
+
 class InvalidProjectId(Exception):
     """Raised when the project_id is invalid"""
-    pass
+    message = "Couldn't find any project with the specified id."
+    tb = traceback.format_exc()
 
 
 class ProjectNotFound(Exception):
     """"Raised when a project is not found"""
-    pass
+    message = "Couldn't find the specified project."
+    tb = traceback.format_exc()
 
 
 class ToolUnavailable(Exception):
     """"Raised when a Tool is unavailable at the time"""
-    pass
+    message = "Couldn't reach the tool service."
+    tb = traceback.format_exc()
 
 
 class InvalidCredentials(Exception):
     """"Raised when invalid credentials is provided"""
-    pass
+    message = "Invalid Credentials please try again."
+    tb = traceback.format_exc()
